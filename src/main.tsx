9750aaed893d68b9fa1efc927291dc2bb381a5b3
Emmanuel <okponobiemmanuel@gmail.com>
2024-06-18 07:07:06 +0100

    setup remote repo

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
+import "normalize.css"
+import './index.css'
+
+import App from './App.tsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
